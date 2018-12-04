ced1ede69e7c44592e2a7ce4af3e2935921e9b44
jisen <289736032@qq.com>
2018-12-04 16:50:52 +0800

    update

---
+++ src/main/java/com/jisen/minappshop/MinappshopApplication.java
@@ -0,0 +1,12 @@
+package com.jisen.minappshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinappshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinappshopApplication.class, args);
+    }
+}
